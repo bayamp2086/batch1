90c64d5d58ac222c4ae7de770715068a4d2daf18
Harish Appannagari <github@bayamp.com>
2019-03-09 16:17:11 -0800

    JIRA : http://www.jira.com/jira-1234

--- basicjava/src/sonia/practicealgo/test.java
+++ basicjava/src/sonia/practicealgo/test.java
@@ -5,7 +5,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("adding this feature that will help restore peace in the world");
 	}
 
 }
