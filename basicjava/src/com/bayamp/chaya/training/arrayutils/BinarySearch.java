91c9ef96517e69af0f2111f109441d165b8f97b6
BayAmp Technologies <github@bayamp.com>
2019-03-10 16:08:12 -0700

    no message

---
+++ basicjava/src/com/bayamp/chaya/training/arrayutils/BinarySearch.java
@@ -0,0 +1,36 @@
+package com.bayamp.chaya.training.arrayutils;
+
+public class BinarySearch {
+	public int recursiveBinarySearch(int[] array, int leftIndex, int rightIndex, int num) {
+		if (rightIndex >= leftIndex) {
+			int mid = leftIndex + (rightIndex - leftIndex) / 2;
+
+			if (array[mid] == num)
+				return mid;
+
+			if (array[mid] > num)
+				return recursiveBinarySearch(array, leftIndex, mid - 1, num);
+			else
+				return recursiveBinarySearch(array, mid + 1, rightIndex, num);
+
+		}
+		return -1;
+	}
+
+	public int iterativeBinarySearch(int[] array, int num) {
+		int leftIndex = 0;
+		int rightIndex = array.length - 1;
+		while (leftIndex <= rightIndex) {
+			int mid = leftIndex + (rightIndex - leftIndex) / 2;
+			if (array[mid] == num)
+				return mid;
+			if (array[mid] > num)
+				rightIndex = mid - 1;
+			else
+				leftIndex = mid + 1;
+		}
+		return -1;
+
+	}
+
+}
---
+++ basicjava/src/com/bayamp/chaya/training/driver/CountLengthOfStringMain.java
@@ -0,0 +1,13 @@
+package com.bayamp.chaya.training.driver;
+
+import com.bayamp.chaya.training.string.util.CountLengthOfString;
+
+public class CountLengthOfStringMain {
+
+	public static void main(String[] args) {
+		CountLengthOfString str = new CountLengthOfString();
+		str.countLength("chaya");
+
+	}
+
+}
