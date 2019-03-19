83719488dd9d48be835ecdc9fbcf8a9c20d8dafb
BayAmp Technologies <github@bayamp.com>
2019-03-19 11:46:32 -0700

    Commited in shape util

--- basicjava/src/com/bayamp/chaya/training/shapeutils/DiamondShapePrint.java
+++ basicjava/src/com/bayamp/chaya/training/shapeutils/DiamondShapePrint.java
@@ -3,26 +3,26 @@ package com.bayamp.chaya.training.shapeutils;
 public class DiamondShapePrint {
 
 	public static void shape(int n) {
-		int mid= (n+1)/2;
-		for (int i = 1; i <=mid; i++) {
-			for(int space=1;space<=mid-i;space++){
+		int mid = (n + 1) / 2;
+		for (int i = 1; i <= mid; i++) {
+			for (int space = 1; space <= mid - i; space++) {
 				System.out.print(" ");
 			}
-			for(int star=1;star<=(i*2)-1;star++){
+			for (int star = 1; star <= (i * 2) - 1; star++) {
 				System.out.print("*");
 			}
 			System.out.println();
-			
+
 		}
-		for (int i = 1; i <=mid-1; i++) {
-			for(int space=1;space<=i;space++){
+		for (int i = 1; i <= mid - 1; i++) {
+			for (int space = 1; space <= i; space++) {
 				System.out.print(" ");
 			}
-			for(int star=1;star<=2*(mid-i)-1;star++){
+			for (int star = 1; star <= 2 * (mid - i) - 1; star++) {
 				System.out.print("*");
 			}
 			System.out.println();
-			
+
 		}
 	}
 
