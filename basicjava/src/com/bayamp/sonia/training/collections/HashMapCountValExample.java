e1e48f17060d7be0a6db9d5bbe524a1cf54f45b0
Harish <github@bayamp.com>
2019-03-20 23:46:53 -0700

    HashMap Sorting and Creating HashMap from Array Number count and String Count

---
+++ basicjava/src/com/bayamp/sonia/training/collections/HashMapCountValExample.java
@@ -0,0 +1,31 @@
+package com.bayamp.sonia.training.collections;
+
+import java.util.Map;
+
+import com.bayamp.sonia.training.utils.CollectionsUtils;
+
+public class HashMapCountValExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String[] fruitArray = { "apple", "apple", "banana", "cherry", "apple", "apple", "cherry", "strawberry", "apple",
+				"cherry", "banana" };
+
+		Map<String, Integer> fruitMap = CollectionsUtils.getFruitArrayCountMap(fruitArray);
+
+		System.out.println(fruitMap);
+
+		int[] numbArray = { 4, 3, 4, 4, 4, 3, 6, 7, 1, 2, 3, 4, 5, 7, 1, 1, 1 };
+
+		Map<String, Integer> numberMap = CollectionsUtils.getNumberArrayCountMap(numbArray);
+		System.out.println(numberMap);
+
+		String str = "abcdabcdabcdabcdab";
+
+		String outPutStr = CollectionsUtils.getCharCountString(str);
+		System.out.println(outPutStr);
+
+	}
+
+}
