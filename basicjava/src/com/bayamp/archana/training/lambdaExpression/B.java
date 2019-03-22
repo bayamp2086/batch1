9f0364b84b01356ebea5afba7784fd380dfd672c
Harish <github@bayamp.com>
2019-03-22 13:49:45 -0700

    Lambda Expression

---
+++ basicjava/src/com/bayamp/archana/training/lambdaExpression/B.java
@@ -0,0 +1,11 @@
+package com.bayamp.archana.training.lambdaExpression;
+
+/*public class B implements A {
+
+	@Override
+	public void show() {
+		System.out.println("Hello");
+		
+	}
+
+}*/
---
+++ basicjava/src/com/bayamp/archana/training/lambdaExpression/LambdaExpression.java
@@ -0,0 +1,38 @@
+package com.bayamp.archana.training.lambdaExpression;
+
+public class LambdaExpression {
+	public static void main(String[] args) {
+		// only in java 8 
+
+		/*A obj = new B();
+		obj.show();*/
+
+
+		/*A obj = new A() 
+		{	public void show() {
+			System.out.println("Hello");
+		}
+		};
+		obj.show();*/		
+
+
+
+		/*A obj = () ->
+				{
+			System.out.println("Hello");
+		};
+         obj.show();*/
+
+
+        /* A obj = () -> System.out.println("Hello");
+		obj.show();*/
+		
+		
+		// pass arguments in interface
+		A obj = (i) -> System.out.println("Hello " +i);
+		obj.show(6);
+		
+		//obj.ShowArg(i);
+
+	}
+}
