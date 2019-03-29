b241a8a52c8bf927b8de932d514cf12e19271544
BayAmp Technologies <github@bayamp.com>
2019-03-29 12:02:49 -0700

    no message

--- mavenjava/src/main/java/com/bayamp/chaya/training/testng/PasswordRegexTestNG.java
+++ mavenjava/src/main/java/com/bayamp/chaya/training/testng/PasswordRegexTestNG.java
@@ -7,16 +7,16 @@ import com.bayamp.chaya.training.regularexpression.PasswordRegex;
 
 public class PasswordRegexTestNG {
 
-	@Test(priority=0,dataProvider = "positivePasswordCheck", dataProviderClass = com.bayamp.chaya.training.dataproviderutility.PasswordRegexDataProvider.class)
+	@Test(priority = 0, dataProvider = "positivePasswordCheck", dataProviderClass = com.bayamp.chaya.training.dataproviderutility.PasswordRegexDataProvider.class)
 	public void positivePasswordValidation(String passwordData, boolean expected) {
 		System.out.println(passwordData + ": " + expected);
 		Assert.assertEquals(expected, PasswordRegex.isPasswordValid(passwordData));
 	}
 
-	@Test(priority=1,dataProvider = "negativePasswordCheck", dataProviderClass = com.bayamp.chaya.training.dataproviderutility.PasswordRegexDataProvider.class)
+	@Test(priority = 1, dataProvider = "negativePasswordCheck", dataProviderClass = com.bayamp.chaya.training.dataproviderutility.PasswordRegexDataProvider.class)
 	public void negativePasswordValidation(String passwordData, boolean expected) {
 		boolean actualResult = PasswordRegex.isPasswordValid(passwordData);
 		System.out.println(passwordData + ": " + expected);
-		Assert.assertEquals(expected,actualResult);
+		Assert.assertEquals(expected, actualResult);
 	}
 }
